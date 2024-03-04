e3cb4be34cf2554db28e5a74d5fad60befb0dd10
jeongbaebang <jeongbaebang_dev@naver.com>
2024-03-04 14:04:14 +0900

    update: 프로젝트 네이밍 수정

--- ios/rnlabTests/rnlabTests.m
+++ ios/rnlabTests/rnlabTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface navigationTests : XCTestCase
+@interface rnlabTests : XCTestCase
 
 @end
 
-@implementation navigationTests
+@implementation rnlabTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
