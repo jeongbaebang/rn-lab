abc9a4108e90515e26d2a40d35ff651df456521c
jeongbaebang <jeongbaebang_dev@naver.com>
2024-04-27 15:04:14 +0900

    add: devDependencies

---
+++ ios/SwiftUIView.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  rnlab
+//
+//  Created by jeongbae bang on 4/27/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
+
